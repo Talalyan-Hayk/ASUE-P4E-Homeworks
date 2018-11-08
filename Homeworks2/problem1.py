308b588a6aa727bef5300dd4bb62ccdde0fc96b9
Hayk <talalyanhayk@gmail.com>
2018-11-08 16:44:46 +0200

    updated Homework2

--- Homeworks2/problem1.py
+++ Homeworks2/problem1.py
@@ -5,7 +5,8 @@ num = int(input("Enter an integer: "))
 
 
 
-if: num%2 == 0
+
+if num % 2 == 0
   print ("The  number is even")
 else: 
   print ("The number is odd ")  
--- Homeworks2/problem2.py
+++ Homeworks2/problem2.py
@@ -1,17 +1,30 @@
 import random
 num = random.randint(1, 100)
 while True:
-    print('Guess a number between 1 and 100')
+
+
+    print('Guess a number between 1 and 100 and if you Want to exit type 999 ')
+
+
+
     guess = input()
     i = int(guess)
     if i == num:
         print('You won!!!')
-        break
+        break 
+    elif i == 999:
+               break
     elif i < num:
                print('Try Higher')
     elif i > num:
                print('Try Lower')
-
+    
+    
+               
+                  
+    
+               
+    		
 
 
 print('if you gussed less than 5  times you won')
